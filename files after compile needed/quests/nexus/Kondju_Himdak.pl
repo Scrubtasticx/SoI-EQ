4bac15e6f851c1da339527440715198352d8a9f6
Andrew Kirk <scrubtasticx@gmail.com>
2020-09-12 11:26:55 -0400

    Update Kondju_Himdak.pl

--- files after compile needed/quests/nexus/Kondju_Himdak.pl
+++ files after compile needed/quests/nexus/Kondju_Himdak.pl
@@ -1,40 +1,44 @@
 sub EVENT_SAY {
-    my %responses = (1 => "The next item you need is an eye from an evil unicorn in Lesser Faydark.");
     
-    if ($text =~/Hail/i && $ulevel >= 60 && !defined $qglobals{"Epic-1.5"}) {
+    if ($text =~/Hail/i && !defined $qglobals{"Epic-1.5"}) {
         plugin::Whisper("Hello $name, along my journies I have acquired knowledge on how to create a " . quest::saylink("Weapon", 1) . " with power like none seen before.");
-    } elsif ($text =~/Hail/i && $ulevel <= 59 && !defined $qglobals{"Epic-1.5"}) {
-        plugin::Whisper("Hello $name, I'm sorry but you must be level 60 to start your 1.5");
+    } elsif ($text =~/Hail/i && defined $qglobals{"Epic-1.5"}) {
+        plugin::Whisper("Hello $name, I'm sorry but you have your epic 1.5");
     } 
     if ($text=~/^Weapon$/i) {
-        plugin::Whisper("In order for me to make you this special weapon I need a orb from a vampire.");
+        plugin::Whisper("In order for me to make you this special weapon I need a orb from a vampire, and you epic 1.0.");
     }
 }
 
 sub EVENT_ITEM {
-    my %handins = (1 => [147565]);
-    my %rewards = ("Warrior" => 60332, 
-    "Cleric" => 20076, 
-    "Paladin" => 48147, 
-    "Ranger" => 62649, 
-    "Shadowknight" => 48136,
-    "Druid" => 62880, 
-    "Monk" => 67742, 
-    "Bard" => 77640, 
-    "Rogue" => 52347, 
-    "Shaman" => 57405, 
-    "Necromancer" => 64067, 
-    "Wizard" => 16576, 
-    "Magician" => 19839, 
-    "Enchanter" => 52962, 
-    "Beastlord" => 52911, 
-    "Berserker" => 18609);
+
+	%ItemDB = (
+	60332 => { "ReqItem1" => 147565, "ReqItem2" => 10908}, #Warrior
+	20076 => { "ReqItem1" => 147565, "ReqItem2" => 5532}, #Cleric
+	48147 => { "ReqItem1" => 147565, "ReqItem2" => 10099}, #Paladin
+	62649 => { "ReqItem1" => 147565, "ReqItem2" => 20487}, #Ranger
+	48136 => { "ReqItem1" => 147565, "ReqItem2" => 14383}, #Shadowknight
+	62880 => { "ReqItem1" => 147565, "ReqItem2" => 20490}, #Druid
+	67742 => { "ReqItem1" => 147565, "ReqItem2" => 10652}, #Monk
+	77640 => { "ReqItem1" => 147565, "ReqItem2" => 20542}, #Bard
+	52347 => { "ReqItem1" => 147565, "ReqItem2" => 11057}, #Rogue
+	57405 => { "ReqItem1" => 147565, "ReqItem2" => 10651}, #Shaman
+	64067 => { "ReqItem1" => 147565, "ReqItem2" => 20544}, #Necromancer
+	16576 => { "ReqItem1" => 147565, "ReqItem2" => 14341}, #Wizard
+	19839 => { "ReqItem1" => 147565, "ReqItem2" => 28034}, #Magician
+	52962 => { "ReqItem1" => 147565, "ReqItem2" => 10650}, #Enchanter
+	52911 => { "ReqItem1" => 147565, "ReqItem2" => 8495}, #Beastlord
+	18609 => { "ReqItem1" => 147565, "ReqItem2" => 68299}, #Berserker
+);
+	foreach $id (sort keys %ItemDB)
+			{
     if (!defined $qglobals{"Epic-1.5"}) {
-        if (plugin::check_handin(\%itemcount, 147565 => 1)) {
+        if(plugin::check_handin(\%itemcount, $ItemDB{$id}{"ReqItem1"} => 1, $ItemDB{$id}{"ReqItem2"} => 1)) {
             quest::summonitem($rewards{$class});
             quest::setglobal("Epic-1.5", 1, 5, "F");
 			plugin::Whisper("Congrats on your weapon.");
         }
-    } 
+    }
+}	
     plugin::return_items(\%itemcount);
 }
