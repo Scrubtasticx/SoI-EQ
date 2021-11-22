5de94059df843c323f56ed9fa0828d2445111a01
Andrew Kirk <scrubtasticx@gmail.com>
2021-11-22 14:03:09 -0500

    Update Overlord_Mata_Muram.pl

--- quests/anguish/Overlord_Mata_Muram.pl
+++ quests/anguish/Overlord_Mata_Muram.pl
@@ -1,6 +1,6 @@
 sub EVENT_DEATH_COMPLETE {
   quest::stoptimer(1);
   quest::ze(1,"Loud cries of hopelessness echo throughout the burning lands. The creatures of Anguish call out to their master, Overlord Mata, for his dead body now lies at the feet of the mighty adventurers.");
-  quest::spawn2(217058,0,0,$x,$y,$z,$h); #Progression Oracle
+  quest::spawn2(2000075,0,0,$x,$y,$z,$h); #Progression Oracle
 }
 # End of File  Zone: Anguish  ID: 217054  -- Mata
